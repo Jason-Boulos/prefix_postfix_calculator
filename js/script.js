ff46461035ad482aba19dc19a32fcf51466adc17
0xjayb <jasonboulos0@gmail.com>
2024-12-18 11:59:05 +0200

    js css <> add switch between prefix and postfix mode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // global variables
+
 const theme_changer_btn = document.getElementById("dark/light-mode");
-const display =  document.getElementById("display")
+const display =  document.getElementById("display");
+const prefixModeBtn = document.getElementById('prefixMode');
+const postfixModeBtn = document.getElementById('postfixMode');
 
 
 
@@ -25,6 +28,27 @@ function appendToDisplay(value){
 function clearDisplay() {
     display.value = '';
 }
+
+// set prefix mode as active by default
+prefixModeBtn.classList.add('active');
+postfixModeBtn.classList.remove('active');
+
+//function to switch between prefix and postfix mode
+function switchMode(btn){
+    if( prefixModeBtn === btn){
+       prefixModeBtn.classList.add('active');
+        postfixModeBtn.classList.remove('active')
+        clearDisplay()
+    }
+    else{
+        postfixModeBtn.classList.add('active');
+       prefixModeBtn.classList.remove('active')
+       clearDisplay()
+    }
+}
+
+prefixModeBtn.addEventListener('click',()=>switchMode(prefixModeBtn));
+postfixModeBtn.addEventListener('click',() =>switchMode(postfixModeBtn))
    
 
 
